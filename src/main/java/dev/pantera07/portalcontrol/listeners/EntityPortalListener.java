265bfeb079b2e96666e40be745a2bc144c30e4a6
BuildTools <unconfigured@null.spigotmc.org>
2023-08-06 22:57:53 +0900

    Updated Source

--- src/main/java/dev/pantera07/portalcontrol/listeners/EntityPortalListener.java
+++ src/main/java/dev/pantera07/portalcontrol/listeners/EntityPortalListener.java
@@ -3,13 +3,13 @@ package dev.pantera07.portalcontrol.listeners;
 import org.bukkit.entity.Ghast;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import io.papermc.paper.event.entity.EntityPortalReadyEvent;
+import org.bukkit.event.entity.EntityPortalEvent;
 
 import dev.pantera07.portalcontrol.PortalControl;
 
 public class EntityPortalListener implements Listener {
     @EventHandler
-    public void on(final EntityPortalReadyEvent event) {
+    public void on(final EntityPortalEvent event) {
         if (event.getEntity() instanceof Ghast) event.setCancelled(true);
     }
 }
